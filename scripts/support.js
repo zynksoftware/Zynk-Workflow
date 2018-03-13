a4115ad0190126c09fc3aa4921a9fd9d70337df5
Adam McCrory <adam@internetware.co.uk>
2018-03-13 10:34:44 +0000

    Create support.js

---
+++ scripts/support.js
@@ -0,0 +1,12 @@
+        <script>!function (e, o, n) { window.HSCW = o, window.HS = n, n.beacon = n.beacon || {}; var t = n.beacon; t.userConfig = {}, t.readyQueue = [], t.config = function (e) { this.userConfig = e }, t.ready = function (e) { this.readyQueue.push(e) }, o.config = { docs: { enabled: !1, baseUrl: "" }, contact: { enabled: !0, formId: "16c803dc-26a2-11e8-b466-0ec85169275a" } }; var r = e.getElementsByTagName("script")[0], c = e.createElement("script"); c.type = "text/javascript", c.async = !0, c.src = "https://djtflbt20bdde.cloudfront.net/", r.parentNode.insertBefore(c, r) }(document, window.HSCW || {}, window.HS || {});</script>
+        <script>
+            HS.beacon.config({
+                color: '#c00',
+                icon: 'message',
+                poweredBy: false,
+                showName: true,
+                showSubject: true,
+                attachment: true,
+               // instructions: 'This is instructional text that goes above the form.'
+            });
+        </script>
